7825a2e4e64353e017bf476042c7a8ab7882d1d2
shivajiveera <shivajiveera12@gmail.com>
2023-01-29 10:55:40 +0530

    adding the file in the server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <React.Fragment>
+    <h1 className=" text-center my-5 text-danger text-capitalize">
+       shopping
+        </h1>
+        <div className="container">
+        <div className="row">
+        <div className="col-sm">
+        <div className = "d-flex justify-content-center align-items center">
+        
+        <div className="card" style={{width: "18rem"}}>
+        <img  
+        src="https://tse1.mm.bing.net/th?id=OIP.zQV2tS49nOJ9ysFtBy7gfAHaIq&pid=Api&P=0"
+        className="card-img-top"
+        alt="..."
+        height="150px"/>
+        
+        
+         <div className="card-body">
+         <h5 className="card-title text ">card tittle</h5>
+         <p>white long dress. </p> ⭕ 🔘<br/>4.8/5★★★☆☆
+         
+         </div>
+        </div>
+        </div>
+        </div>
+
+        <div className="col-sm">
+        <div className = "d-flex justify-content align-items-center ">
+        <div className="card" style={{width: "18rem"}}>
+        <img
+        src="https://cdn.walletmonitor.com/img/86ebe90c41b99ed9953c56abd1f1fb0a.jpg"
+        className="card-img-top"
+        alt="..."
+        height="150px"/>🤍
+          <div className="card-body">
+          <h5 className="card-title ">card tittle</h5>
+          <p className="card-text">
+          Full Sleeve Denim Jacket
+          </p>⭕ 🔘<br/>4.8/5★★★☆☆
+        
+      
+        
+          </div>
+        </div>
+        </div>
+        </div>
+
+        <div className="col-sm">
+        <div className="d-flex justify-content-center align-items center">
+        <div className="card" style={{width: "18rem"}}>
+        <img 
+        src="https://tse2.mm.bing.net/th?id=OIP.L8k0jB3bLyc6HFj5bhBzqwHaGz&pid=Api&P=0"
+        className="card-img-top"
+        alt="..."
+        height="150px"/> 
+         
+        <div className="cart">
+        🤍
+        </div>
+
+
+
+
+        <div className="card-body">
+        <h5 className="card-tittle">Card tittle</h5>
+        <p className="card-text">Hush puls
+        </p>⭕ 🔘<br/>4.8/5★★★☆☆
+       
+        </div>
+        </div>
+        </div>
+        
+        </div>
+        </div>
+        </div>
+    </React.Fragment>
   );
 }
 
